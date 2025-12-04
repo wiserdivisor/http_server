23e8b1dff16e8b0087c4ca384a5854e75ac13f43
wiserdivisor <dat0user@gmail.com>
2025-12-05 00:35:53 +0530

    python server 1.0 unfinished

--- http_server.py
+++ http_server.py
@@ -1,3 +1,4 @@
+import os
 import time
 from email.utils import formatdate
 import socket
@@ -5,6 +6,7 @@ from datetime import datetime
 
 host = "127.0.0.1"
 port = 8080
+root = os.path.abspath("www")
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # create socket
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # set options
@@ -30,12 +32,21 @@ while True:
     print("\n---- raw request END ----")
     lines = request_text.split("\r\n")
     request_line = lines[0]
-    method, raw_uri, http_version = request_line.split(" ")
-    if not method or not raw_uri or http_version not in ("HTTP/1.1", "HTTP/1.0"):
+    request_line_tokens = request_line.split(" ")
+    if(len(request_line_tokens) < 3):
         response = "HTTP/1.0 400 Bad Request\r\n\r\n<h1>400 Bad Request</h1>"
         conn.sendall(response.encode("latin-1"))
         conn.close()
         continue
+    
+    method = request_line_tokens[0]
+    if(method not in ("GET", "POST", "HEAD")):
+        body = b"<h1>501 Not Implemented.</h1>"
+        status = "HTTP/1.0 501 Not Implemented\r\n"
+   
+    # FIND FILE USING URI AND RETURN ELSE 404 
+    raw_uri = request_line_tokens[1]
+
     headers = {}
     for line in lines[1:]:
         if not line:
@@ -45,9 +56,14 @@ while True:
     print(f"Method: {method}\r\nRaw URI: {raw_uri}\r\nHTTP Version: {http_version}")
     print("headers",headers)
 
-    status_line = "HTTP/1.0 200 OK\r\n"
+    status_code = ""
+    status_message = ""
+    
+    status_line = f"HTTP/1.0 {status_code} {status_message}\r\n"
     date_header = f"Date: {formatdate(timeval=time.time(), localtime=False, usegmt=True)}\r\n"
     server_header = "Server: VREDDYHTTP/1.0\r\n"
+
+    
 
     body = b"<h1>Hello from VREDDY HTTP/1.0 Server.</h1><br><h1>This server is compliant with RFC-1945</h1>"
     content_type = "Content-Type: text/html; charset=utf-8\r\n"
